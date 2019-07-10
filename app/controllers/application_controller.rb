ce65d89b9b3229225f1cab2a39b69aa9fed105c6
Ariel <arielord@buffalo.edu>
2019-07-10 04:36:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,36 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+  
+  post '/articles' do
+    article = Article.create(title: params['title'], content: params['content'])
+    
+    redirect 'articles/' + article.id.to_s
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    
+    erb :article
+  end
+  
+  get '/articles/:id/edit' do
+    
+    
+    erb :edit
+  end
+  
+  
 end
